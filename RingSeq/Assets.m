ca9a4897e65f346702a8b94379e64edd89c0340e
Nir Boneh <nboneh5921@gmail.com>
2014-12-12 15:28:03 -0700

    Organizing all Asset files

--- RingSeq/Assets.m
+++ RingSeq/Assets.m
@@ -16,65 +16,63 @@
 static NSArray* ERASE_SOUNDS;
 static NSArray *INSTRUMENTS;
 static NSArray *IN_APP_PURCHASE_PACKS;
-+(void) initialize{
++(void) load{
     // Mute all audio if the silent switch is turned on.
     [OALSimpleAudio sharedInstance].honorSilentSwitch = YES;
+    NSMutableArray *instrumentMut = [[NSMutableArray alloc] init];
+    //Some of the more bassy instruments are tuned to different octaves to let them play bass sounds in treble cleft
+    //C4
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Acoustic Guitar" color: [UIColor redColor] andBaseOctave:4]];
+    //C4
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Electric Guitar" color: [UIColor blueColor] andBaseOctave:4]];
+    [instrumentMut addObject:[[Drums alloc] initWithName:@"Drums" color:[UIColor brownColor] andBaseOctave:4]];
+    //C4
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Bass Guitar" color:[UIColor greenColor] andBaseOctave:4 ]];
+    //C5
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Bell" color:[UIColor cyanColor] andBaseOctave:5]];
+    //C5
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Trumpet" color:[UIColor yellowColor] andBaseOctave:5]];
+    //C3
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Trombone" color:[UIColor magentaColor]  andBaseOctave:4]];
+    //C5
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Saxophone" color:[UIColor orangeColor] andBaseOctave:5]];
+    //C5
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Orchestra Hit" color:[UIColor grayColor] andBaseOctave:5]];
+    
+    //C4
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"High Piano" color:[UIColor blackColor]andBaseOctave:4]];
+    //C2
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Low Piano" color:[UIColor blackColor]  andBaseOctave:4]];
+    //C5
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"Clarinet" color:[UIColor cyanColor]  andBaseOctave:5]];
+    //C5
+    [instrumentMut addObject:[[Instrument alloc] initWithName:@"String Section" color:[UIColor brownColor]  andBaseOctave:5]];
+    
+    
+    
+    NSArray * packs =[Assets IN_APP_PURCHASE_PACKS];
+    for(NSDictionary * pack in packs){
+        NSArray * instruments = [pack objectForKey:@"instruments"];
+        for(Instrument *instrument in instruments){
+            [instrumentMut addObject:instrument];
+        }
+    }
+    
+    
+    INSTRUMENTS  = [[NSArray alloc]initWithArray:instrumentMut];
+    
+    ERASE_SOUNDS = [[NSArray alloc] initWithObjects:@"delete1.wav", @"delete2.wav", @"delete3.wav",@"delete4.wav", nil];
+
     
 }
 
 +(NSArray *)INSTRUMENTS{
-    if(!INSTRUMENTS){
-        NSMutableArray *instrumentMut = [[NSMutableArray alloc] init];
-        //Some of the more bassy instruments are tuned to different octaves to let them play bass sounds in treble cleft
-        //C4
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Acoustic Guitar" color: [UIColor redColor] andBaseOctave:4]];
-        //C4
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Electric Guitar" color: [UIColor blueColor] andBaseOctave:4]];
-        [instrumentMut addObject:[[Drums alloc] initWithName:@"Drums" color:[UIColor brownColor] andBaseOctave:4]];
-        //C4
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Bass Guitar" color:[UIColor greenColor] andBaseOctave:4 ]];
-        //C5
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Bell" color:[UIColor cyanColor] andBaseOctave:5]];
-        //C5
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Trumpet" color:[UIColor yellowColor] andBaseOctave:5]];
-        //C3
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Trombone" color:[UIColor magentaColor]  andBaseOctave:4]];
-        //C5
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Saxophone" color:[UIColor orangeColor] andBaseOctave:5]];
-        //C5
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Orchestra Hit" color:[UIColor grayColor] andBaseOctave:5]];
-        
-        //C4
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"High Piano" color:[UIColor blackColor]andBaseOctave:4]];
-        //C2
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Low Piano" color:[UIColor blackColor]  andBaseOctave:4]];
-        //C5
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"Clarinet" color:[UIColor cyanColor]  andBaseOctave:5]];
-        //C5
-        [instrumentMut addObject:[[Instrument alloc] initWithName:@"String Section" color:[UIColor brownColor]  andBaseOctave:5]];
-
-
-        
-        NSArray * packs =[Assets IN_APP_PURCHASE_PACKS];
-        for(NSDictionary * pack in packs){
-            NSArray * instruments = [pack objectForKey:@"instruments"];
-            for(Instrument *instrument in instruments){
-                [instrumentMut addObject:instrument];
-            }
-        }
-        
-        
-        
-        INSTRUMENTS  = [[NSArray alloc]initWithArray:instrumentMut];
-    }
+    
     return INSTRUMENTS;
 }
 
 
 +(void) playEraseSound{
-    if(!ERASE_SOUNDS){
-        ERASE_SOUNDS = [[NSArray alloc] initWithObjects:@"delete1.wav", @"delete2.wav", @"delete3.wav",@"delete4.wav", nil];
-    }
     
     [[OALSimpleAudio sharedInstance] playEffect:[ERASE_SOUNDS objectAtIndex: arc4random_uniform((int)ERASE_SOUNDS.count)]];
 }
